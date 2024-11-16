760884d4012a37f42c9c15b6900655adc3e922ff
Fabian Jankowski <fjankowsk@gmail.com>
2024-11-17 00:22:43 +0100

    Try debugging.

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,41 +32,41 @@ SOCIAL = (
     ("Another social link", "#"),
 )
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
-# add date to output filenames and drafts
-ARTICLE_URL = "{date:%Y-%m-%d}.{slug}/"
-ARTICLE_SAVE_AS = "{date:%Y-%m-%d}.{slug}/index.html"
-DRAFT_URL = "drafts/{date:%Y-%m-%d}.{slug}/"
-DRAFT_SAVE_AS = "drafts/{date:%Y-%m-%d}.{slug}/index.html"
+# # add date to output filenames and drafts
+# ARTICLE_URL = "{date:%Y-%m-%d}.{slug}/"
+# ARTICLE_SAVE_AS = "{date:%Y-%m-%d}.{slug}/index.html"
+# DRAFT_URL = "drafts/{date:%Y-%m-%d}.{slug}/"
+# DRAFT_SAVE_AS = "drafts/{date:%Y-%m-%d}.{slug}/index.html"
 
-# use separate directories for pages and categories
-PAGE_URL = "{slug}/"
-PAGE_SAVE_AS = "{slug}/index.html"
-CATEGORY_URL = "category/{slug}/"
-CATEGORY_SAVE_AS = "category/{slug}/index.html"
+# # use separate directories for pages and categories
+# PAGE_URL = "{slug}/"
+# PAGE_SAVE_AS = "{slug}/index.html"
+# CATEGORY_URL = "category/{slug}/"
+# CATEGORY_SAVE_AS = "category/{slug}/index.html"
 
-# use nice urls for tags
-TAG_URL = "tag/{slug}/"
-TAG_SAVE_AS = "tag/{slug}/index.html"
+# # use nice urls for tags
+# TAG_URL = "tag/{slug}/"
+# TAG_SAVE_AS = "tag/{slug}/index.html"
 
-# we don't need the authors page
-AUTHOR_SAVE_AS = ""
+# # we don't need the authors page
+# AUTHOR_SAVE_AS = ""
 
-# archives
-YEAR_ARCHIVE_SAVE_AS = "{date:%Y}/index.html"
+# # archives
+# YEAR_ARCHIVE_SAVE_AS = "{date:%Y}/index.html"
 
-# use nice urls here as well
-ARCHIVES_URL = "archives/"
-ARCHIVES_SAVE_AS = "archives/index.html"
-AUTHORS_SAVE_AS = ""
-CATEGORIES_URL = "categories/"
-CATEGORIES_SAVE_AS = "categories/index.html"
-TAGS_URL = "tag/"
-TAGS_SAVE_AS = "tag/index.html"
+# # use nice urls here as well
+# ARCHIVES_URL = "archives/"
+# ARCHIVES_SAVE_AS = "archives/index.html"
+# AUTHORS_SAVE_AS = ""
+# CATEGORIES_URL = "categories/"
+# CATEGORIES_SAVE_AS = "categories/index.html"
+# TAGS_URL = "tag/"
+# TAGS_SAVE_AS = "tag/index.html"
 
-# use nice urls for pagination as well
-PAGINATION_PATTERNS = (
-    (1, "{base_name}/", "{base_name}/index.html"),
-    (2, "{base_name}/page/{number}/", "{base_name}/page/{number}/index.html"),
-)
+# # use nice urls for pagination as well
+# PAGINATION_PATTERNS = (
+#     (1, "{base_name}/", "{base_name}/index.html"),
+#     (2, "{base_name}/page/{number}/", "{base_name}/page/{number}/index.html"),
+# )
